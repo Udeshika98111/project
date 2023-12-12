613a623f8bb8e6525ff4f361891177c5e8764011
Udeshika Sethunga <smsusethunga@gmail.com>
2023-12-12 16:51:15 +0530

    1st_commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Project__Nano_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
